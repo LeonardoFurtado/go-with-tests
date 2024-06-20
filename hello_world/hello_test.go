b145c732211c5490ea715339ff48e8f078029bca
Leonardo Furtado <srleonardofurtado@gmail.com>
2024-06-20 04:24:19 -0300

    final hello world.

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,14 +3,29 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to peole", func(t *testing.T) {
-		got := Hello("Chris")
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Saying hello in french", func(t *testing.T) {
+		got := Hello("Louis", "French")
+		want := "Bonjour, Louis"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Saying hello in portuguese", func(t *testing.T) {
+		got := Hello("Leo", "Portuguese")
+		want := "Olá, Leo"
 		assertCorrectMessage(t, got, want)
 	})
 }
